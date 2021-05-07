cb47405cf3f2bbbf409844d9ac9029556d27b631
widmo <widmo@widmo.pl>
2021-05-07 23:49:52 +0200

    Initial commit

---
+++ src/com/medowski/Main.java
@@ -0,0 +1,12 @@
+package com.medowski;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("1 line");
+        System.out.println("2 line");
+        System.out.println("3 line");
+
+    }
+}
